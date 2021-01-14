9905e2c814c18b0710f5ef9c36eff0dc5ddcf4e8
chiraggoyal19 <cgoyal98@gmail.com>
2021-01-15 02:29:09 +0530

    Test-5Three numbers with Comma & NewLine delimiter return sum-REFACTORED

--- TDDKataStringCalculator/src/Calculator.java
+++ TDDKataStringCalculator/src/Calculator.java
@@ -1,16 +1,13 @@
 
 public class Calculator {
-
+	private final String delimiter=",|\n";
 	public int calculate(String input) {
-		String numbers[]=input.split(",|\n");
+		String numbers[]=input.split(delimiter);
 		if(input.isEmpty()) {
 			return 0;
 		}
 		if(numbers.length==1 && input.length()==1) {
 			return Integer.parseInt(input);
-		}
-		if(numbers.length==3) {
-			return 6;
 		}
 		else {
 			return CalculateSum(numbers);
@@ -19,7 +16,9 @@ public class Calculator {
 	
 	private int CalculateSum(String number[]) {
 		int sum=0;
-		sum+=Integer.parseInt(number[0])+Integer.parseInt(number[1]);
+		for(String current:number) {
+			sum+=Integer.parseInt(current);
+		}
 		return sum;
 	}
 
