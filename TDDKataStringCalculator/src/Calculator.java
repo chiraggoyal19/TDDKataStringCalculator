2b65915b34340d0d075876af67fa1cc2ef150936
chiraggoyal19 <cgoyal98@gmail.com>
2021-01-15 00:52:27 +0530

    Test-1 Empty String must return Zero-Failure

--- TDDKataStringCalculator/src/Calculator.java
+++ TDDKataStringCalculator/src/Calculator.java
@@ -1,4 +1,9 @@
 
 public class Calculator {
 
+	public int calculate(String string) {
+		
+		return -1;
+	}
+
 }
--- TDDKataStringCalculator/src/TestCalculate.java
+++ TDDKataStringCalculator/src/TestCalculate.java
@@ -1,7 +1,13 @@
 import org.testng.annotations.Test;
 import static org.testng.Assert.assertEquals;
-
 @Test
 public class TestCalculate {
-
+	
+	
+	//Test-1 Empty String must return Zero
+	public void EmptyStringReturnZero(){
+		Calculator calculator=new Calculator();
+		assertEquals(calculator.calculate(""),0);
+		
+	}
 }
